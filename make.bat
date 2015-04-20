c1c6784dfa83ecd00e53e32197695a43412967e5
Vyacheslav Lopatynskyy <darkfisk@gmail.com>
2015-04-20 17:35:18 +0300

    add deploy script for windows

---
+++ make.bat
@@ -0,0 +1,3 @@
+d:
+cd d:\apps\addonsdk
+bin\activate.bat & c: & cd C:\Users\Vyacheslav_Lopatynsk\git\Quizlet-Helper & cfx xpi & START "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" c:\Users\Vyacheslav_Lopatynsk\git\Quizlet-Helper\quizlet-helper.xpi 
