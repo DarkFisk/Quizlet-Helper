b6b35aeee9f4e9effc283c54dd46bf18a62063e9
Vyacheslav Lopatynskyy <darkfisk@gmail.com>
2015-04-23 16:20:20 +0300

    cosmetics

--- data/login.js
+++
@@ -1,7 +0,0 @@
-var button=document.getElementById("submit");
-button.addEventListener("click", function(e){
-	self.postMessage(document.getElementById("code").value);
-	e.preventDefault();
-	window.close();
-	return false;
-});
